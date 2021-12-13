b5c6e329a7407bff5ee5af19217d91960a7fd631
musemy <musemy@naver.com>
2021-12-13 14:22:04 +0900

    store/admin

--- src/store/login.ts
+++ src/store/login.ts
@@ -12,26 +12,24 @@ interface ILoginParams {
     pw: string;
 }
 
-
 const initialState: ILogin = {
     loading: false,
     status: undefined
 }
 
-
-export const postLogin = createAsyncThunk( // init시 호출
+export const postLogin = createAsyncThunk(
     'setup/postLogin',
     async(params: ILoginParams) => {
         try {
             const response = await axios.post(`${REQUEST_URL}/auth/setupLogin`, params);
             console.log(response);
-            // data.code === 200 어쩌구 저쩌구에 따라 return값 다르게 처리가능
             if (response.status === 200) {
                 return 'SUCCESS';
             }
             if (response.status === 250 || response.status === 260) {
                 return 'FAILURE';
-            }// action.payload : http의 모양일것 => IAddSetupData 형태로 바꿔야함
+            }
+            return 'FAILURE';
         } catch(error) {
             console.log('login rejected::', error);
         }
@@ -43,19 +41,19 @@ const loginSlice = createSlice({
     name: 'login',
     initialState,
     reducers: {
-        resetUserLogin: state => { // initialState 로
+        resetUserLogin: () => {
             return initialState;
         },
     },
     extraReducers: (builder) => {
-        builder.addCase(postLogin.fulfilled, (state, action) => { // action.payload는 위의 axios에서 return된 값
+        builder.addCase(postLogin.fulfilled, (state, action) => {
             state.loading = false;
             state.status = action.payload;
         })
         .addCase(postLogin.pending, (state) => {
             state.loading = true;
         })
-        .addCase(postLogin.rejected, (state, action) => {
+        .addCase(postLogin.rejected, (state) => {
             state.loading= false;
         })
     },
