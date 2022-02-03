3bfcfae8dbd79350d6c2229337a2771bd9eae993
musemy <musemy@naver.com>
2022-02-03 13:04:39 +0900

    안쓰는 파일삭제

--- src/components/menu/tabs.tsx
+++ src/components/menu/tabs.tsx
@@ -2,13 +2,11 @@
 export const TabType = {
     SETUP: "SETUP",
     MENU: "MENU",
-    // TAB3: "TAB3",
 };
   
 export const TabLabel = {
     [TabType.SETUP]: "셋업 설정",
     [TabType.MENU]: "메뉴 관리",
-    // [TabType.TAB3]: "탭 3 - 미정",
 };
 
 interface ITabProps {
@@ -21,9 +19,7 @@ export const Tabs = ({ selectedTab = 'SETUP', onChange }: ITabProps) => {
 
     const handleTabClick = (tabType: string) => {
         // TODO TEMP:::TAB DISABLED
-        // if (tabType !== 'TAB3') {
         onChange(tabType)
-        // }
     }
 
     return (
