f58d33417403cf42063966bcfa87ba47666085bc
musemy <musemy@naver.com>
2022-02-03 13:05:24 +0900

    아래 내용과 동일

--- src/components/menu/tab3/tab3.tsx
+++
@@ -1,9 +0,0 @@
-export const Tabs3 = () => {
-    return (
-        <div className="row h-90">
-            <div className="box">
-                <h2>탭3 화면입니다</h2>
-            </div>
-		</div>
-    );
-};
