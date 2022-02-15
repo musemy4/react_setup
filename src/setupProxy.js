c58830ff2d3ca662c8bb570b48163ba5c7691eaf
nanas26 <58456250+musemy4@users.noreply.github.com>
2022-02-16 08:54:32 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,14 +2,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/vurix-dms', {
-      target: 'http://debugca-172-16-36-180.vurix.kr/',
+    createProxyMiddleware('**', {
+      target: '**',
       changeOrigin: true,
     })
   );
   app.use(
-    createProxyMiddleware('/tms-decision', {
-      target: 'http://docs-172-16-36-180.vurix.kr/',
+    createProxyMiddleware('**', {
+      target: '**',
       changeOrigin: true,
     })
   );
